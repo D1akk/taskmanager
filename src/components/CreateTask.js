dfa98b29e9ae2d4dac395a382d5879da59616024
egor dyakonov <egordyakonov2000@gmail.com>
2023-10-10 00:46:06 +0900

    updated some front, next - api connect to db

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function CreateTask() {
-    const [inputs, setInputs] = useState({})
-    const handleChange = (event) 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    } 
-    return (
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:3000/api/task/save", inputs);
+    
+    //console.log(inputs);
+  };
+  return (
     <div>
       <h1>Добавить задачу</h1>
       <form onSubmit={handleSubmit}>
@@ -14,10 +22,10 @@ export default function CreateTask() {
         <input type="text" name="title" onChange={handleChange}></input>
         <br />
         <label>Описание: </label>
-        <input type="text" name="description"></input>
+        <input type="text" name="description" onChange={handleChange}></input>
         <br />
         <label>Ответственный: </label>
-        <input type="text" name="case-staff"></input>
+        <input type="text" name="case-staff" onChange={handleChange}></input>
         <br />
         <button>Создать</button>
       </form>
